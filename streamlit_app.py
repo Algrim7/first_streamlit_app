33e50673be3f9407b18583e3581218e2a6f902d3
Algrim7 <65973259+Algrim7@users.noreply.github.com>
2022-10-13 14:06:33 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,14 @@ from urllib.error import URLError
 
 
 streamlit.title('My Parents New Healthy Diner')
-
+streamlit.stop()
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Bluebarry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.stop()
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
